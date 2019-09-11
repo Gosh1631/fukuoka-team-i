e8d54d8f943fd35fbc80650a6abeb8202ffc7dd0
Gosh1631 <daitaka5606@gmail.com>
2019-09-11 15:31:55 +0900

    jango実装に向けて勉強を兼ねて編集をしています。HalloWorld出せるよ。()

--- oursite/moist/views.py
+++ oursite/moist/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello,world.")
+
+# def response(request):
+#     context = {
+#         if rate < 100:
+#             "humid_res" = "水量が減少しています。"
+
+#         elif rate < 200:
+#             "humid_res" = "良好です。"
+
+#         else:
+#             "humid_res" = "水分量が過多になっています。"
+#     }
+#     return render(request, "moist":"moist.html", context)
