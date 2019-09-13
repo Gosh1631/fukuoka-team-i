348c17120e40f8df4412be2afbeb68cc92f1989c
Gosh1631 <daitaka5606@gmail.com>
2019-09-13 13:25:14 +0900

    シリアル通信の追加と、htmlにif文を追加するための準備に取り掛かり始めました。

--- oursite/moist/views.py
+++ oursite/moist/views.py
@@ -1,18 +1,21 @@
 from django.http import HttpResponse
+import serial,time
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello,world.")
 
-# def response(request):
-#     context = {
-#         if rate < 100:
-#             "humid_res" = "水量が減少しています。"
+def response(request):
+    ser = serial.Serial("/dev/ttyUSB0",9600)
+    if rate < 100:
+        int status = 0
+        ser.write(1)
 
-#         elif rate < 200:
-#             "humid_res" = "良好です。"
+    elif rate < 200:
+        int status = 1
+        ser.write(0)
 
-#         else:
-#             "humid_res" = "水分量が過多になっています。"
-#     }
-#     return render(request, "moist":"moist.html", context)
+    else:
+        int status = 2
+        ser.write(0)
+    return render(request, "moist":"moist.html", context)
