11887d7df42fcb6f245bc6d128965ac477a3fe46
harukihirota <haruki120306b@icloud.com>
2019-09-13 16:30:48 +0900

    u

--- oursite2/coin_cal/models.py
+++ oursite2/coin_cal/models.py
@@ -5,3 +5,7 @@ class Coin(models.Model):
     class Meta:
         db_table = 'money'
     sum_of_coin = models.IntegerField()
+class Documents(model.Model):
+    description = models.CharField(max_length=255, blank=True)
+    photo = models.ImageField(upload_to='media/', default='defo')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
