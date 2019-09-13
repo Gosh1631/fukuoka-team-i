c086ed803c14dea475fb994fdc2930ca750fe13d
Gosh1631 <daitaka5606@gmail.com>
2019-09-14 02:13:46 +0900

    バグを取っていました。 あと、macでの動作を想定した通信方法に変更しています。

--- oursite/moist/views.py
+++ oursite/moist/views.py
@@ -9,24 +9,21 @@ import serial,time
 
 class index(View):
     template_name = "moist.html"
-    def response(request):
-        ser = serial.Serial("COM8",9600)
+    class response(View):
+        ser = serial.Serial("/dev/cu.usbmodem14401",9600)
         rate = ser.readline()
         params = {
             "water_level": rate,
         }
-        judge(rate)
-        return render(request,"moist.html",params)
+        if rate() < 100:
+            int.status = 0
+            ser.write(1)
 
-def judge(rate):
-    if rate < 100:
-        int.status = 0
-        ser.write(1)
+        elif rate() < 200:
+            int.status = 1
+            ser.write(0)
 
-    elif rate < 200:
-        int.status = 1
-        ser.write(0)
-
-    else:
-        int.status = 2
-        ser.write(0)
+        else:
+            int.status = 2
+            ser.write(0)
+        # return render(request,"moist.html",params)
