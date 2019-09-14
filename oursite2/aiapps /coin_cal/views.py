5de9b2981a7984d018b5a1590506e4f6ccff52b3
harukihirota <haruki120306b@icloud.com>
2019-09-14 22:27:05 +0900

    change file

--- oursite2/aiapps /coin_cal/views.py
+++ oursite2/aiapps /coin_cal/views.py
@@ -1,12 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
-
+from .forms import Photoform 
 # Create your views here.
 def index(request):
   template = loader.get_template('coin_cal/index.html')
-  context = {'form'}
-  return HttpResponse("Hello World!")
+  context = {'form':PhotoForm()}
+  return HttpResponse(tempate.render(context, request))
 
 def predict(request):
-  return HttpResponse('Show result')
+  if not request.method == 'Post'
+    return
+    redirect('coin_cal:index')
+
+  form = PhotoForm(request.Post, request.FILES)
+  if not form.is_valid():
+    raise ValueError('Formが不正です')
+
+  photo = Photo(image=form.cleaned_data[''])
+
+  return HttpResponse()
