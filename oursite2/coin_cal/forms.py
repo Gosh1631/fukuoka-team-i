52c16a57ab9788a34c442add510f4260057f6f1c
harukihirota <haruki120306b@icloud.com>
2019-09-14 21:11:09 +0900

    hu

--- oursite2/coin_cal/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Document
-
-class PhotoFrom(forms.Form):
-    image = forms.ImageField()
-class DocumentForm(form.ModelForm):
-    class Meta:
-        model = Document
-        fields = ('description', 'photo')
---
+++ oursite2/venv/aiapps/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\realm\desktop\fukuoka-team-i\oursite2\venv\aiapps\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
